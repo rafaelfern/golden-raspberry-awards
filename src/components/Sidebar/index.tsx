7c0a1605a93f3088b2d2c0c9e024efc4723a1630
rafaelfern <rafaelaugfern@gmail.com>
2024-05-09 14:54:39 -0300

    wip: including active style in sidebar active link

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './index.scss';
 import { Home, List } from 'react-feather';
 interface ISidebar {
@@ -7,7 +7,7 @@ interface ISidebar {
 
 function Sidebar(props: ISidebar) {
   const { screenWidth } = props;
-
+  const location = useLocation();
   return (
     <>
       {screenWidth <= 768 ? (
@@ -22,11 +22,15 @@ function Sidebar(props: ISidebar) {
       ) : (
         <div data-testid="sidebar" className="sidebar">
           <ul>
-            <li role="button" className="btn-link">
-              <Link to={'dashboard'}>Dashboard</Link>
+            <li role="button">
+              <Link to={'dashboard'} className={location.pathname === '/dashboard' ? 'btn-link active' : 'btn-link'}>
+                Dashboard
+              </Link>
             </li>
             <li role="button">
-              <Link to={'list'}>List</Link>
+              <Link to={'list'} className={location.pathname === '/list' ? 'active' : ''}>
+                List
+              </Link>
             </li>
           </ul>
         </div>
