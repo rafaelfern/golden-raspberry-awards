cd6ee3b7a9e581e067be5ad5ca92b0eba976f1e7
rafaelfern <rafaelaugfern@gmail.com>
2024-05-09 14:20:56 -0300

    wip: Adjusting button icons when screen is less than 768px

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import './index.scss';
-
+import { Home, List } from 'react-feather';
 interface ISidebar {
   screenWidth: number;
 }
@@ -13,19 +13,10 @@ function Sidebar(props: ISidebar) {
       {screenWidth <= 768 ? (
         <div data-testid="sidebar" className="sidebar">
           <button role="button" onClick={() => window.location.assign('dashboard')} className="button">
-            <svg xmlns="http://www.w3.org/2000/svg" className="icon-svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M3 3h18v18H3V3zm5 8a1 1 0 110-2 1 1 0 010 2zM3 12h6M13 8h8M13 16h8"
-              />
-            </svg>
+            <Home />
           </button>
           <button onClick={() => window.location.assign('list')} className="button">
-            <svg xmlns="http://www.w3.org/2000/svg" className="icon-svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 10h16M4 14h16M4 18h16" />
-            </svg>
+            <List />
           </button>
         </div>
       ) : (
